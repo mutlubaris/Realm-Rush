8b515753e70faef328060f8d04d4c4555819a3e2
mutlubaris <mutlubaris_1@hotmail.com>
2019-08-26 11:35:46 +0300

    revising coroutines

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    void Start()
+    {
+        Pathfinder pathFinder = FindObjectOfType<Pathfinder>();
+        var path = pathFinder.GetPath();
+        StartCoroutine(FollowPath(path));
+    }
+
+    IEnumerator FollowPath(List<Waypoint> path)
+    {
+        print("Starting patrol.");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(2f);
+        }
+        print("Ending patrol.");
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, new Vector3(-10, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
